ef2187036c499a51fe7d0e04341ab41a4c8dd78e
ndelanhese <nnathanh3@gmail.com>
2023-09-15 16:32:17 -0300

    feat(sidebar): created searchbar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,7 +1,9 @@
 import { Logo } from '../icons/logo'
+import { SearchBar } from './searchBar'
 
 export const Sidebar = () => (
-  <aside className="border-r border-zinc-200 px-5 py-8">
+  <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
     <Logo />
+    <SearchBar />
   </aside>
 )
